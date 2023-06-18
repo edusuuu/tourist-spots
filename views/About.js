11bf0f4fce619a35062a151edaae4bb5bcf8a8e3
nefarious222222222 <135100337+nefarious222222222@users.noreply.github.com>
2023-06-18 17:38:35 +0800

    Google API implemented

--- views/About.js
+++ views/About.js
@@ -7,7 +7,7 @@ fetch('Students.xml')
     function updateStudentProfiles() {
       const studentNames = document.querySelectorAll('.studentName');
       const studentAges = document.querySelectorAll('.studentAge');
-      const studentGenders = document.querySelectorAll('.studentGender');
+      const studentYearSec = document.querySelectorAll('.studentYearSec');
       const studentAddresses = document.querySelectorAll('.studentAddress');
 
       const profileElements = xmlDoc.getElementsByTagName('profile');
@@ -16,12 +16,12 @@ fetch('Students.xml')
         const profile = profileElements[i];
         const name = profile.querySelector('name').textContent;
         const age = profile.querySelector('age').textContent;
-        const gender = profile.querySelector('gender').textContent;
+        const yearSec = profile.querySelector('yearSec').textContent;
         const address = profile.querySelector('address').textContent;
 
         studentNames[i].textContent = name;
         studentAges[i].textContent = age;
-        studentGenders[i].textContent = gender;
+        studentYearSec[i].textContent = yearSec;
         studentAddresses[i].textContent = address;
       }
     }
@@ -31,3 +31,39 @@ fetch('Students.xml')
   .catch(error => {
     console.error('Error fetching or parsing XML:', error);
   });
+
+//Map features
+var map;
+
+function initMap() {
+  var initialLocation = { lat: 14.673602028301183, lng: 120.50175047837489 }; // Default location (Balanga, Bataan)
+
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: initialLocation,
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var locations = document.getElementsByClassName("aboutUs");
+  for (var i = 0; i < locations.length; i++) {
+    locations[i].addEventListener("click", zoomToLocation);
+  }
+}
+
+function zoomToLocation() {
+  var lat = parseFloat(this.querySelector(".studentProfile").getAttribute("data-lat"));
+  var lng = parseFloat(this.querySelector(".studentProfile").getAttribute("data-lng"));
+  var location = { lat: lat, lng: lng };
+
+  map.setZoom(14); // Adjust the desired zoom level here
+  map.panTo(location); // Center the map to the clicked location
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    title: "Student's Location"
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
